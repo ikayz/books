7b4fda22fb52b2acd1e6559a8c4863a5ab932a6b
Isaac Miti <ikayz@live.com>
2020-07-05 23:58:50 +0200

    Add catch block for malformed URLs

--- app/src/main/java/com/ikayz/books/ApiUtil.java
+++ app/src/main/java/com/ikayz/books/ApiUtil.java
@@ -1,4 +1,22 @@
 package com.ikayz.books;// Created by ikayz on 05/07/2020
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ApiUtil {
+    private ApiUtil() {}
+
+    public static final String BASE_API_URL =
+            "https://www.googleapis.com/books/v1/volumes";
+
+    public static URL buildUrl(String title) {
+        String fullUrl = BASE_API_URL + "?q=" + title;
+        URL url = null;
+        try {
+            url = new URL(fullUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
 }
