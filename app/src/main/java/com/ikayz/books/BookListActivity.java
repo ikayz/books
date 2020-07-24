f73b2d8c294749b5b98a5dead90ae8d0c02b92c0
Isaac Miti <ikayz@live.com>
2020-07-24 16:35:54 +0200

    Add progress bar

--- app/src/main/java/com/ikayz/books/BookListActivity.java
+++ app/src/main/java/com/ikayz/books/BookListActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -12,10 +14,13 @@ import java.net.URL;
 
 public class BookListActivity extends AppCompatActivity {
 
+    private ProgressBar mLoadingProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
             new BooksQueryTask().execute(bookUrl);
@@ -42,7 +47,14 @@ public class BookListActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             TextView tvResult = findViewById(R.id.tv_response);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
             tvResult.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
 }
