1a4541af0ba6d38ea29dc71a9816c720a424f04c
Isaac Miti <ikayz@live.com>
2020-07-24 16:21:50 +0200

    Add API key and implement constant class to hold it

--- app/src/main/java/com/ikayz/books/ApiUtil.java
+++ app/src/main/java/com/ikayz/books/ApiUtil.java
@@ -9,19 +9,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-public class ApiUtil {
-    private static final String QUERY_PARAMETER_KEY = "q";
+import static com.ikayz.books.ApiKey.API_KEY;
 
+public class ApiUtil {
     private ApiUtil() {}
 
     public static final String BASE_API_URL =
             "https://www.googleapis.com/books/v1/volumes";
+    private static final String QUERY_PARAMETER_KEY = "q";
+    public static final String KEY = "key";
 
     public static URL buildUrl(String title) {
 
         URL url = null;
         Uri uri = Uri.parse(BASE_API_URL).buildUpon()
                 .appendQueryParameter(QUERY_PARAMETER_KEY, title)
+                .appendQueryParameter(KEY, API_KEY)
                 .build();
         try {
             url = new URL(uri.toString());
